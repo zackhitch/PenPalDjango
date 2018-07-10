65d5a1874ad6b93fc9b1e5a3204886289e77cfd7
Kaitlyn Schorr <kaitlynschorr@gmail.com>
2018-07-10 14:45:49 -0500

    CREATE BLANK COMPONENTS
    - determined necessary components & created placemarker files
    - installed react router and react strap

---
+++ letters/src/components/Letter.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Letter extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default Letter;
---
+++ letters/src/components/PenPal.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PenPal extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default PenPal;
