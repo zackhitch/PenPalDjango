ef9985f4b2f7348a59767a0e045226e828a007c3
Kaitlyn Schorr <kaitlynschorr@gmail.com>
2018-07-10 15:26:50 -0500

    FIX HEROKU DEPLOY ERRORS

--- PenPalDjango/settings.py
+++ PenPalDjango/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from decouple import config
+
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
