57b8d7272c2cbdc247f507a516eef8176cda73be
Kaitlyn Schorr <kaitlynschorr@gmail.com>
2018-07-11 21:07:44 -0500

    FIX USER PERMISSIONS
    - change permissions setting to IsAuthenticated

--- PenPalDjango/settings.py
+++ PenPalDjango/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {'DEFAULT_AUTHENTICATION_CLASSES': (
 ),
 
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
 ]
 }
 
